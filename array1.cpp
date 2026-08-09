3a8aa38dcb1ced2f093ceff353f056deebd0c350
Amna Shoukat <153503548+AmnaShokat@users.noreply.github.com>
2026-08-09 21:01:03 +0500

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10], size;
+    cout << "enter size of array: ";
+    cin >> size;
+    cout << "enter the element: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "elements in array are: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
