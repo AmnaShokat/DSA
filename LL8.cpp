ecd06a1cfd0849e9dc3cab1f29184b452e272363
Amna Shoukat <153503548+AmnaShokat@users.noreply.github.com>
2026-09-24 10:09:59 +0500

    Add files via upload

---
+++ LL8.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+};
+node *head = nullptr;
+void insertAtStart(int value)
+{
+    node *newnode = new node();
+    newnode->data = value;
+    newnode->next = head;
+    head = newnode;
+}
+void reverse()
+{
+    node *temp = head;
+    node *prev = nullptr;
+
+    while (temp != nullptr)
+    {
+        node *nextnode = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = nextnode;
+    }
+    head = prev;
+}
+void display()
+{
+    node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    insertAtStart(10);
+    insertAtStart(20);
+    insertAtStart(30);
+    insertAtStart(40);
+    insertAtStart(50);
+    insertAtStart(60);
+    insertAtStart(70);
+    insertAtStart(80);
+    reverse();
+
+    display();
+}
